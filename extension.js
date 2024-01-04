4bceec0b83bcfc4f7c8e978a9febb52449ff650c
Gil Gourevitch <2204051+gilgourevitch@users.noreply.github.com>
2024-01-04 14:35:09 +0100

    add all 4 commands

--- extension.js
+++ extension.js
@@ -17,14 +17,57 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('apex-log-utilities.helloWorld', function () {
+	let openLogStreamCmd = vscode.commands.registerCommand('apex-log-utilities.openLogStream', function () {
 		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from apex-log-utilities!');
+		let terminal = getTerminal('Apex Logs Streaming');
+		terminal.show();
+		terminal.sendText("sf apex tail log --color");
 	});
 
-	context.subscriptions.push(disposable);
+	let openDebugStreamCmd = vscode.commands.registerCommand('apex-log-utilities.openDebugStream', function () {
+		// The code you place here will be executed every time your command is executed
+		let terminal = getTerminal('Apex Debug Streaming');
+		terminal.show();
+		terminal.sendText("sf apex tail log --color | grep USER_DEBUG");
+	});
+
+	let openLogStreamInTabCmd = vscode.commands.registerCommand('apex-log-utilities.openLogStreamInTab', function () {
+		// The code you place here will be executed every time your command is executed
+		let terminal = getTerminal('Apex Logs Streaming');
+		// terminal.show();
+		terminal.show();
+		terminal.sendText("sf apex tail log --color | code -");
+
+	});
+
+	let openDebugStreamInTabCmd = vscode.commands.registerCommand('apex-log-utilities.openDebugStreamInTab', function () {
+		// The code you place here will be executed every time your command is executed
+		let terminal = getTerminal('Apex Logs Streaming');
+		// terminal.show();
+		terminal.show();
+		terminal.sendText("sf apex tail log --color | grep USER_DEBUG | code -");
+		
+	});
+
+	context.subscriptions.push(openLogStreamCmd);
+	context.subscriptions.push(openDebugStreamCmd);
+	context.subscriptions.push(openLogStreamCmdInTab);
+	context.subscriptions.push(openDebugStreamCmdInTab);
+}
+
+function getTerminal(terminalName){
+	let terminal;
+	vscode.window.terminals.forEach(term => {
+		if(term.name == terminalName){
+			terminal = term;
+		}
+	});
+
+	if(!terminal){
+		terminal = vscode.window.createTerminal(terminalName);
+	}
+	return terminal;
+
 }
 
 // This method is called when your extension is deactivated
